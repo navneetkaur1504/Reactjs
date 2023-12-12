34488d6b646a7acb776c4b9b0093dc4f7f0d5187
navneetkaur1504 <navneetkaur150403@gmail.com>
2023-12-12 23:59:05 +0530

    created reduxtoolkit

---
+++ reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -0,0 +1,24 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+    todos: [{id:1, text: "Hello world"}]
+}
+
+
+
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState,
+    reducers: {
+        addTodo: (state, action) => {
+            const todo = {
+                id: nanoid(),
+                text: action.payload
+            }
+            state.todos.push(todo)
+        },
+        removeTodo: (state, action) => {
+            
+        },
+    }
+})
